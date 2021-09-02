27a0616be10ed88e7bfa6909efa9eb9b39213e6d
iagocq <18238046+iagocq@users.noreply.github.com>
2021-09-02 05:22:41 -0300

    lightspeed-ingest: fix warning

--- pkgs/lightspeed-ingest/default.nix
+++ pkgs/lightspeed-ingest/default.nix
@@ -1,2 +1,2 @@
-{ pkgs, lib, stdenv, buildRustCrate, defaultCrateOverrides }: (import ./Cargo.nix { inherit pkgs lib stdenv buildRustCrate defaultCrateOverrides; }).workspaceMembers.lightspeed-ingest.build
+{ pkgs, lib, stdenv }: (import ./Cargo.nix { inherit pkgs lib stdenv; }).workspaceMembers.lightspeed-ingest.build
 
