6f6db099de77b626fe308216f41d32f2482275ef
iagocq <18238046+iagocq@users.noreply.github.com>
2022-03-03 19:15:52 -0300

    pkgs: tweak parsec drv

--- pkgs/parsec/default.nix
+++ pkgs/parsec/default.nix
@@ -1,8 +1,8 @@
 # based on https://github.com/clefru/nur-packages/blob/master/pkgs/parsecgaming/default.nix
 
-{ buildFHSUserEnv, fetchurl, curl, jq, writeScript
-, stdenv, dpkg, alsa-lib, cups, dbus, fontconfig
-, freetype, libGL, libsamplerate
+{ lib, stdenv, buildFHSUserEnv, fetchurl, copyDesktopItems
+, curl, jq, writeScript, dpkg, alsa-lib, cups, dbus, fontconfig
+, freetype, libGL, libsamplerate, makeDesktopItem
 , libudev, libva, vaapiVdpau, libxkbcommon, nas
 , vulkan-loader, libX11, libXScrnSaver, libXcursor
 , libXext, libXi, libXinerama, libXrandr, libXrender
@@ -12,7 +12,7 @@
 let
   version = "150-78";
 
-  debExtract = stdenv.mkDerivation {
+  src = stdenv.mkDerivation {
     name = "parsec-pkg";
     src = fetchurl {
       url = "https://s3.amazonaws.com/parsec-build/package/parsec-linux.deb";
@@ -65,6 +65,7 @@ let
     popd
     /usr/bin/parsecd
   '';
+
   linkAlsaConf = stdenv.mkDerivation {
     name = "parsec-alsa-conf";
     buildCommand = ''
@@ -73,28 +74,56 @@ let
       ln -s /host/etc/alsa alsa
     '';
   };
-in buildFHSUserEnv {
-  name = "parsec";
-  targetPkgs = pkgs: [
-    alsa-lib cups dbus debExtract fontconfig
-    freetype libGL libsamplerate
-    libudev libva vaapiVdpau libxkbcommon nas
-    vulkan-loader libX11 libXScrnSaver libXcursor
-    libXext libXi libXinerama libXrandr libXrender
-    libXxf86vm libxcb openssl stdenv.cc.cc.lib
-    linkAlsaConf
-    # Those libraries are missing from buildInputs
-    # libGLES_CM.so.1        # Can't find in my /nix/store
-    # libudev.so.0           # Covered by libudev.so.1
-    # libva.so.1             # Covered by libva.so.2
-    # libwayland-client.so.0 # No wayland support
-    # libwayland-cursor.so.0 # No wayland support
-    # libwayland-egl.so.1    # No wayland support
-  ] ++ extraPkgs;
-  runScript = launchScript;
+
+  fhsEnv = buildFHSUserEnv {
+    name = "parsec-fhs-wrapper";
+    targetPkgs = pkgs: [
+      alsa-lib cups dbus src fontconfig
+      freetype libGL libsamplerate
+      libudev libva vaapiVdpau libxkbcommon nas
+      vulkan-loader libX11 libXScrnSaver libXcursor
+      libXext libXi libXinerama libXrandr libXrender
+      libXxf86vm libxcb openssl stdenv.cc.cc.lib
+      linkAlsaConf
+      # Those libraries are missing from buildInputs
+      # libGLES_CM.so.1        # Can't find in my /nix/store
+      # libudev.so.0           # Covered by libudev.so.1
+      # libva.so.1             # Covered by libva.so.2
+      # libwayland-client.so.0 # No wayland support
+      # libwayland-cursor.so.0 # No wayland support
+      # libwayland-egl.so.1    # No wayland support
+    ] ++ extraPkgs;
+    runScript = launchScript;
+  };
+in stdenv.mkDerivation rec {
+  pname = "parsec";
+  inherit version;
+
+  nativeBuildInputs = [ copyDesktopItems ];
+  desktopItems = [ (makeDesktopItem {
+    name = "parsec";
+    desktopName = "Parsec";
+    comment = "Simple, low-latency game streaming.";
+    exec = "parsec %u";
+    icon = "parsecd";
+    categories = "Network;Game;Utility;";
+  }) ];
+
+  phases = [ "installPhase" ];
+  installPhase = ''
+    mkdir $out
+    runHook preInstall
+    mkdir $out/bin
+    ln -s ${fhsEnv}/bin/parsec-fhs-wrapper $out/bin/parsec
+    for d in ${src}/share/icons/hicolor/*; do
+      install -Dm0644 -t $out/share/icons/hicolor/$(basename $d)/apps/ $d/apps/parsecd.png
+    done
+    runHook postInstall
+  '';
 
   meta = {
-    description = "Parsec Daemon";
-    homepage = "https://parsec.app/";
+    description = "Simple, low-latency game streaming.";
+    homepage = "https://parsec.app";
+    license = with lib.licenses; [ unfree ];
   };
 }
